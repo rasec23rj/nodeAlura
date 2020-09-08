3f653e53f2b274ebedfc124116a3b7b97ab83432
Júlio César <rasec23rj@gmail.com>
2020-09-08 17:30:06 -0300

    rota index

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const consign = require('consign')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+
+consign()
+  .include('model')
+  .then('controller')
+  .then('root')
+  .into(app);
  
 app.listen(3000)
